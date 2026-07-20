6ed311e2811d9c648d29f21455c2cb00aa494b52
David65656 <146118730+David65656@users.noreply.github.com>
2026-07-20 23:35:57 +0200

    Add files via upload

---
+++ core_basic_window.c
@@ -0,0 +1,59 @@
+#include "raylib.h"
+
+#define SCREEN_WIDTH  1280
+#define SCREEN_HEIGHT  720
+
+#define CHARACTER_HEIGHT  720
+
+//Szöveg középre igazítása
+void DrawTextCenter(const char *text, int fontSize, Color color)
+{
+   
+    int textWidth = MeasureText(text, fontSize);
+
+    DrawText(
+         text,
+         SCREEN_WIDTH / 2 - textWidth / 2,
+         SCREEN_HEIGHT / 2 - fontSize / 2,
+         fontSize,
+         color
+    );
+}
+
+int main (){
+   
+   //Képernyőablak létrehozása
+   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Super Mario");
+   
+   Texture2D character = LoadTexture("resources/textures/character/characterStand.png");
+  
+   int height = character.height;
+   int width = character.width;
+   
+   float characterScale = (float)CHARACTER_HEIGHT/(float)height;
+   
+   float x = SCREEN_WIDTH / 2.0f - (width * characterScale) / 2.0f;
+   float y = SCREEN_HEIGHT / 2.0f - (height * characterScale) / 2.0f;
+   
+   SetTargetFPS(60);
+   
+   while (!WindowShouldClose())
+   {
+       BeginDrawing();
+       
+       ClearBackground(RAYWHITE);
+       
+       //Karakter kirajzolása középre
+       DrawText(TextFormat("szélesség: %f/ magasság: %f", width * characterScale, height * characterScale), 0, 0, 30, BLACK);
+       DrawTextureEx(character, (Vector2){x, y}, 0.0f, characterScale, WHITE);
+       
+       EndDrawing();
+   }
+   
+   UnloadTexture(character);
+   
+   CloseWindow();
+
+   return 0;
+
+}
